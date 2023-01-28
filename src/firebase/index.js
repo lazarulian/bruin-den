4770c06757a81ce3c2cc4692165a8f7a83f04627
Apurva Shah <shahh.apurva@gmail.com>
2023-01-28 12:42:26 -0800

    Added firestore to project

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import Firebase from 'Firebase/app';
-import 'Firebase/auth';
+import Firebase from "Firebase/app";
+import { getFirestore } from "firebase/firestore";
+import "Firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,15 +16,16 @@ const firebaseConfig = {
   storageBucket: "bruin-den.appspot.com",
   messagingSenderId: "178181796892",
   appId: "1:178181796892:web:814d18c1bac457b8efdbd9",
-  measurementId: "G-7SEJH6ELPK"
+  measurementId: "G-7SEJH6ELPK",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 const analytics = getAnalytics(app);
 
 if (!Firebase.apps.length) {
-    Firebase.initializeApp(FirebaseCredentials)
-  }
-  
+  Firebase.initializeApp(FirebaseCredentials);
+}
+
 export default Firebase;
