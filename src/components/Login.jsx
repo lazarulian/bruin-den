dc7dcfdf12ed83a9ffbf1f6cb4921d6b640118a7
Ays <aalvi@Ayeshas-MacBook-Pro.local>
2023-01-28 12:34:40 -0800

    Login work

---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='p-8 m-8 justify-center'>
+        <h1 className='text-3xl font-semibold'>Login Here</h1>
+        <p>Cheeks are here</p>
+    </div>
+  )
+}
+
+export default Login
