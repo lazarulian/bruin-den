f9957b30111837f2f6b70dcf6fd1a78ff706ebb6
Apurva Shah <shahh.apurva@gmail.com>
2023-01-28 13:56:20 -0800

    Remove broken firebase

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -63,8 +63,8 @@ const logout = () => {
   signOut(auth);
 };
 
-if (!Firebase.apps.length) {
-  Firebase.initializeApp(FirebaseCredentials);
-}
+// if (!Firebase.apps.length) {
+//   Firebase.initializeApp(FirebaseCredentials);
+// }
 
-export default Firebase;
+// export default Firebase;
