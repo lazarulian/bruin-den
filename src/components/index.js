629c5b1773b0613a0f1ae5db64bc4dc6960d9a44
Apurva Shah <shahh.apurva@gmail.com>
2023-01-28 13:41:16 -0800

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
 export { default as Header } from "./Header";
 export { default as Layout } from "./Layout";
-export { default as Login } from "./Login";
 export { default as Apartments } from "./Home/Apartments";
 export { default as Banner } from "./Home/Banner";
 export { default as Couches } from "./Home/Couches";
 export { default as Landing } from "./Landing";
+
 export { default as Table } from "./Apartments/Table";
 export { default as TableEntry } from "./Apartments/TableEntry";
 export { default as AddApartment } from "./Apartments/AddApartment";
