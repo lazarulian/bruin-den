b300282144e945f53479f13c375afeb24796539a
Apurva Shah <shahh.apurva@gmail.com>
2023-01-28 12:40:44 -0800

    fixed bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import Link from 'next/link'
+import Link from "next/link";
 import styles from "@/styles/Home.module.css";
 import { Login } from "@/components";
-\
 import { Landing } from "../components/";
 
 export default function Home() {
@@ -14,4 +13,3 @@ export default function Home() {
     </div>
   );
 }
-
