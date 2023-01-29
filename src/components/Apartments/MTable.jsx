289fa8e4f73a6193a49245678fd4d3470e92d230
Apurva Shah <shahh.apurva@gmail.com>
2023-01-28 16:38:05 -0800

    Fixed Bug

--- src/components/Apartments/MTable.jsx
+++ src/components/Apartments/MTable.jsx
@@ -21,13 +21,14 @@ import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Switch from "@mui/material/Switch";
-import DeleteIcon from "@mui/icons-material/Delete";
 import FilterListIcon from "@mui/icons-material/FilterList";
 import { visuallyHidden } from "@mui/utils";
 
 const MTable = () => {
   const [apartments, setApartments] = useState([]);
   const apartmentsCollectionRef = collection(db, "apartments");
+
+  const [newSearch, setNewSearch] = useState("");
 
   useEffect(() => {
     const getApartments = async () => {
@@ -40,6 +41,40 @@ const MTable = () => {
 
   return (
     <div className="text-center mx-8 my-2 py-6 bg-slate-50 rounded-lg h-screen">
+      <div>
+        <form class="flex items-center">
+          <label for="simple-search" class="sr-only">
+            Search
+          </label>
+          <div class="relative w-full">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg
+                aria-hidden="true"
+                class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </div>
+            <input
+              type="text"
+              id="simple-search"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search"
+              onChange={(event) => {
+                setNewSearch(event.target.value);
+              }}
+            />
+          </div>
+        </form>
+      </div>
+
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
