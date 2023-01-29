a50a5aad032d244393ff170eb3db15bb1bbfa75f
Apurva Shah <shahh.apurva@gmail.com>
2023-01-28 21:28:47 -0800

    added apartment filtering

--- src/components/Apartments/Filters.jsx
+++ src/components/Apartments/Filters.jsx
@@ -1,9 +1,101 @@
 import React from "react";
+import Link from "next/link";
+import {
+  FaRoad,
+  FaRainbow,
+  FaShuttleVan,
+  FaSkiing,
+  FaCouch,
+  FaFire,
+  FaCity,
+  FaShieldAlt,
+  FaWind,
+  FaSatellite,
+} from "react-icons/fa";
 
 const Filters = () => {
   return (
-    <div className="bg-sky-900 w-1/3">
-      <h1>Cheeks</h1>
+    <div className="container w-1/3 bg-sky-100 rounded-lg m-4 p-4">
+      <Link href="/streets/kelton">
+        <button class="bg-sky-800 hover:opacity-50 duration-300 text-white py-1 px-3 rounded-full">
+          <div className="flex items-center justify-center">
+            <FaRoad />
+            &nbsp;Kelton
+          </div>
+        </button>
+      </Link>
+      <Link href="/">
+        <button class="bg-sky-800 hover:opacity-50 duration-300 text-white py-1 m-1 px-3 rounded-full">
+          <div className="flex items-center justify-center">
+            <FaRainbow />
+            &nbsp;Midvale
+          </div>
+        </button>
+      </Link>
+      <Link href="/">
+        <button class="bg-sky-800 hover:opacity-50 duration-300 text-white py-1 m-1 px-3 rounded-full">
+          <div className="flex items-center justify-center">
+            <FaShuttleVan />
+            &nbsp;Veteran
+          </div>
+        </button>
+      </Link>
+      <Link href="/">
+        <button class="bg-sky-800 hover:opacity-50 duration-300 text-white py-1 m-1 px-3 rounded-full">
+          <div className="flex items-center justify-center">
+            <FaSkiing />
+            &nbsp;Glenrock
+          </div>
+        </button>
+      </Link>
+      <Link href="/">
+        <button class="bg-sky-800 hover:opacity-50 duration-300 text-white py-1 m-1 px-3 rounded-full">
+          <div className="flex items-center justify-center">
+            <FaCouch />
+            &nbsp;Landfair
+          </div>
+        </button>
+      </Link>
+      <Link href="/">
+        <button class="bg-sky-800 hover:opacity-50 duration-300 text-white py-1 m-1 px-3 rounded-full">
+          <div className="flex items-center justify-center">
+            <FaFire />
+            &nbsp;Roebling
+          </div>
+        </button>
+      </Link>
+      <Link href="/">
+        <button class="bg-sky-800 hover:opacity-50 duration-300 text-white py-1 m-1 px-3 rounded-full">
+          <div className="flex items-center justify-center">
+            <FaCity />
+            &nbsp;Gayley
+          </div>
+        </button>
+      </Link>
+      <Link href="/">
+        <button class="bg-sky-800 hover:opacity-50 duration-300 text-white py-1 m-1 px-3 rounded-full">
+          <div className="flex items-center justify-center">
+            <FaShieldAlt />
+            &nbsp;Ophir
+          </div>
+        </button>
+      </Link>
+      <Link href="/">
+        <button class="bg-sky-800 hover:opacity-50 duration-300 text-white py-1 m-1 px-3 rounded-full">
+          <div className="flex items-center justify-center">
+            <FaWind />
+            &nbsp;Strathmore
+          </div>
+        </button>
+      </Link>
+      <Link href="/">
+        <button class="bg-sky-800 hover:opacity-50 duration-300 text-white py-1 m-1 px-3 rounded-full">
+          <div className="flex items-center justify-center">
+            <FaSatellite />
+            &nbsp;Levering
+          </div>
+        </button>
+      </Link>
     </div>
   );
 };
---
+++ src/pages/streets/kelton.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Filters, MTable } from "../../components";
+import { db } from "../../firebase-config";
+import { collection, getDocs, query } from "firebase/firestore";
+
+const kelton = () => {
+  return (
+    <div className="text-center mb-2 py-6 bg-slate-50 rounded-lg h-screen">
+      <div className="flex justify-center">
+        <h1 className="text-3xl font-semibold">Kelton</h1>
+      </div>
+      <div className="flex justify-center">
+        <Filters />
+      </div>
+      <MTable />
+    </div>
+  );
+};
+
+export default kelton;
