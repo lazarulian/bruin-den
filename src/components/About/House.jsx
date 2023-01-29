c68ddf9fd93c756ea477fffebf52651431b8e6a6
Apurva Shah <shahh.apurva@gmail.com>
2023-01-29 10:15:20 -0800

    Updated bug

--- src/components/About/House.jsx
+++ src/components/About/House.jsx
@@ -1,23 +1,19 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
-import HouseImg from "../../../public/assets/HouseImage.png"
+import HouseImg from "../../../public/assets/houseImage.png";
 
 const House = () => {
   return (
-    <div className='bg-white grid grid-cols-6 gap-2'>
-        <div className = 'col col-span-2' ></div>
+    <div className="bg-white grid grid-cols-6 gap-2">
+      <div className="col col-span-2"></div>
 
-        <div className = 'text-center col col-span-2 flex items-center justify-center'>   
+      <div className="text-center col col-span-2 flex items-center justify-center">
         <Image src={HouseImg} width={200} alt="House" />
-        </div>
+      </div>
 
-
-        <div className='col col-span-2'> </div>
-    
-        
-
+      <div className="col col-span-2"> </div>
     </div>
-  )
-}
+  );
+};
 
-export default House
+export default House;
