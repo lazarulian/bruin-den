f1cfc48bc5ab95d0d59c5285dc2ac3aec9229356
ayeshamalvi <aalvi@g.ucla.edu>
2023-01-28 21:26:43 -0800

    added mission to landing

--- src/components/Home/Couches.jsx
+++ src/components/Home/Couches.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import Image from "next/image";
+import Logo from "../../../public/assets/logo.png"
 
 const Couches = () => {
   return (
-    <div className="bg-orange-500 w-full">
-      <h1>Couches</h1>
+    <div className=" bg-black">
+      <div className=" flex items-center justify-center">
+        <Image src={Logo} height = {300} width={300} alt="Logo"/>
+      </div>
+      <div className="flex justify-center">
+        <h1 className = "text-white m-10"> A qwer hacks project</h1>
+      </div>
     </div>
+    
+
   );
 };
 
--- src/components/Home/Mission.jsx
+++ src/components/Home/Mission.jsx
@@ -1,14 +1,17 @@
+import { BeachAccessSharp } from "@mui/icons-material";
 import React from "react";
+import Image from "next/image";
+import Bear from "../../../public/assets/Bear.png"
 
 const Mission = () => {
   return (
-    <div className="grid grid-cols-2 h-full">
-      <div className="col col-span-1 text-center pt-10 pl-10 pr-10 ">
-        <h1 className="pb-10">OUR MISSION</h1>
-        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+    <div className=" bg-slate-50 flex h-full text-center m-2 justify-center items-center ">
+      <div className="lg:w-1/3 w-1/2">
+        <h1 className ="text-6xl">OUR MISSION</h1>
+        <p className="m-10"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
       </div>
-      <div className="col col-span-1 text-center pt-10 pl-10 pr-10 ">
-        <h1>Part 2</h1>
+      <div className="col col-span-1 object-center p-20">
+        <Image src={Bear} height={450} alt="Bear"/>
       </div>
     </div>
     
