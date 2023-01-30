efd287586837cbbb8d46ceae292b717fe9359456
Apurva Shah <shahh.apurva@gmail.com>
2023-01-30 14:36:18 -0800

    Update search with autocomplete and abstractions for incorrect searching

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 import { db } from "../../firebase-config";
 import { MTable } from "..";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import FormControl from "@mui/material/FormControl";
 
 import {
   Chart as ChartJS,
@@ -17,8 +20,29 @@ import {
 import { Line } from "react-chartjs-2";
 
 const Search = () => {
-  const [search, setSearch] = useState("");
+  const [initials, setInitials] = useState([]);
   const [apartments, setApartments] = useState([]);
+
+  useEffect(() => {
+    const getInitial = async () => {
+      const q = query(collection(db, "apartments"));
+      const data = await getDocs(q);
+      setInitials(
+        data.docs
+          .map((doc) => ({ ...doc.data(), id: doc.id }))
+          .map((initial) => initial.address)
+          .filter(onlyUnique)
+      );
+    };
+    getInitial();
+  }, []);
+
+  const [search, setSearch] = useState(initials[0]);
+  const [inputValue, setInputValue] = React.useState("");
+
+  function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+  }
 
   const getApartments = async (search) => {
     const q = query(
@@ -71,53 +95,32 @@ const Search = () => {
         <h1 className="text-3xl font-semibold">Search</h1>
       </div>
       <div className="flex justify-center">
-        <div className="container bg-sky-100 w-1/4 rounded-lg m-4 p-4">
-          <div>
-            <label
-              for="default-search"
-              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-            >
-              Search
-            </label>
-            <div className="relative">
-              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg
-                  aria-hidden="true"
-                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  ></path>
-                </svg>
-              </div>
-              <input
-                type="text"
-                className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
-                placeholder="540 Kelton Ave..."
-                onChange={(event) => {
-                  setSearch(event.target.value);
-                }}
-              />
-              <button
-                className="text-white absolute right-2.5 bottom-2.5 bg-sky-800 hover:opacity-50 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 duration-300"
-                onClick={() => {
-                  getApartments(search);
-                }}
-              >
-                Search
-              </button>
-            </div>
-          </div>
-        </div>
+        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+          <Autocomplete
+            options={initials}
+            value={search}
+            onChange={(event, newValue) => {
+              setSearch(newValue);
+            }}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            sx={{ width: 300 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Apartments" />
+            )}
+          />
+        </FormControl>
+        <button
+          className="text-white absolute right-2.5 bottom-2.5 bg-sky-800 hover:opacity-50 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 duration-300"
+          onClick={() => {
+            getApartments(search);
+          }}
+        >
+          Search
+        </button>
       </div>
-
       <div className="grid grid-cols-2 gap-2">
         <div className="col col-span-1">
           <div className="mx-4 mt-8 bg-white rounded-lg">
